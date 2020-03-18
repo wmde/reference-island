31c9836087df58ce984442212a4cd6c28d918461
Amir Sarabadani <Ladsgroup@gmail.com>
2020-03-18 23:29:02 +0100

    Set up GitHub actions
    Based on https://help.github.com/en/actions/language-and-framework-guides/using-python-with-github-actions

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-import os
-
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
     readme = f.read()
 
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().split('\n')
 
-def requirements(fname):
-    return [line.strip()
-            for line in open(os.path.join(os.path.dirname(__file__), fname))]
-
+extras_require = {
+    'tests': [
+        'flake8>=3.2.1',
+        'pytest'
+    ],
+}
 
 setup(
     name="wikidatarefisland",
@@ -20,14 +21,14 @@ setup(
     license="GPLv3",
     url="https://github.com/wmde/reference-island",
     long_description=readme,
-    install_requires=requirements('requirements.txt'),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: GPLv3 License"
     ],
-    packages=['wikidatarefisland'],
-    tests_require=requirements('requirements-dev.txt'),
+    extras_require=extras_require,
+    packages=find_packages(exclude=['*.tests', '*.tests.*']),
     package_dir={'wikidatarefisland': 'wikidatarefisland'},
     python_requires='>=2.7.2, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
 )

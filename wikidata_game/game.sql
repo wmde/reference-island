c834e32c38e13eccf4c669d4e78fde1bf5ac08f0
Amir Sarabadani <Ladsgroup@gmail.com>
2020-04-29 15:48:48 +0200

    Making the api.php work

--- wikidata_game/game.sql
+++ wikidata_game/game.sql
@@ -1,9 +1,9 @@
-CREATE TABLE references (
+CREATE TABLE refs (
   ref_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   ref_data mediumblob NOT NULL,
   -- 0 for unknown, 1 for good, 2 for bad
   ref_flag tinyint NOT NULL DEFAULT 0
-)
+);
 CREATE UNIQUE INDEX ref_data ON references (ref_data);
 -- Covering index
 CREATE INDEX ref_flag ON references (ref_flag, ref_data, ref_id);

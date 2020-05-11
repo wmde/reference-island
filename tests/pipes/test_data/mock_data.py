9d3bc25644d7143cc3d8ad0597c58a878ab5994c
Itamar Givon <itamar.givon.dev@gmail.com>
2020-05-12 01:20:18 +0200

    Rewrite pipe 1 unit test (#38)
    * Rewrite Unit Test
    
    * Cleanup mock data
    
    * Cleanup and lint

---
+++ tests/pipes/test_data/mock_data.py
@@ -0,0 +1,85 @@
+WHITELISTED_EXT_ID = "P3029"
+EXTERNAL_ID_VALUE = "tst1234"
+NON_WHITELISTED_EXT_ID = "P3028"
+ITEM_ID = "Q23"
+BLACKLISTED_PROPERTY = "P26"
+NON_BLACKLISTED_PROPERTY = "P509"
+DATATYPE = "wikibase-item"
+
+VALUE_BLOB = {
+    "entity-type": "item",
+    "numeric-id": 191789,
+    "id": "Q191789"
+}
+
+STATEMENT_BLOB = {
+    "pid": NON_BLACKLISTED_PROPERTY,
+    "datatype": DATATYPE,
+    "value": VALUE_BLOB
+}
+
+RESOURCE_BLOB = {
+    "url": f"http://example.com/{EXTERNAL_ID_VALUE}",
+    "referenceMetadata": None
+}
+
+ITEM = {
+    "pageid": 136,
+    "ns": 0,
+    "title": ITEM_ID,
+    "lastrevid": 1150873645,
+    "modified": "2020-04-05T21:23:54Z",
+    "type": "item",
+    "id": ITEM_ID,
+    "labels": {},
+    "descriptions": {},
+    "aliases": {},
+    "claims": {},
+    "sitelinks": {}
+}
+
+LINE = {
+    "itemId": ITEM_ID,
+    "statements": [],
+    "resourceUrls": []
+}
+
+mock = {
+    "claim": {
+        "with_references": {
+            "mainsnak": {
+                "property": NON_BLACKLISTED_PROPERTY
+            },
+            "references": []
+        },
+        "with_any_external_id": {
+            "mainsnak": {
+                "property": NON_WHITELISTED_EXT_ID,
+                "datatype": "external-id"
+            }
+        },
+        "with_whitelisted_external_id": {
+            "mainsnak": {
+                "datatype": "external-id",
+                "property": WHITELISTED_EXT_ID,
+                "datavalue": {
+                    "value": EXTERNAL_ID_VALUE
+                }
+            }
+        },
+        "with_any_unreferenced_property": {
+            "mainsnak": {
+                "property": NON_BLACKLISTED_PROPERTY,
+                "datavalue": {
+                    "value": VALUE_BLOB
+                },
+                "datatype": DATATYPE
+            }
+        },
+        "with_blacklisted_property": {
+            "mainsnak": {
+                "property": BLACKLISTED_PROPERTY
+            }
+        }
+    }
+}
--- tests/pipes/test_item_extractor_pipe.py
+++ tests/pipes/test_item_extractor_pipe.py
@@ -1,28 +1,101 @@
-import importlib.resources as pkg_resources
-import json
-
 import pytest
 
 from wikidatarefisland.pipes import ItemExtractorPipe
-from . import test_data
+from .test_data import mock_data as test_data
+
+given = {
+    "item": {
+        "without_unreferenced_claims": {
+            **test_data.ITEM,
+            "claims": {
+                test_data.NON_BLACKLISTED_PROPERTY: [
+                    test_data.mock["claim"]["with_references"]
+                ]
+            }
+        },
+        "without_non_blacklisted_properties": {
+            **test_data.ITEM,
+            "claims": {
+                test_data.BLACKLISTED_PROPERTY: [
+                    test_data.mock["claim"]["with_blacklisted_property"]
+                ]
+            }
+        },
+        "without_whitelisted_external_ids": {
+            **test_data.ITEM,
+            "claims": {
+                test_data.NON_BLACKLISTED_PROPERTY: [
+                    test_data.mock["claim"]["with_any_unreferenced_property"]
+                ],
+                test_data.NON_WHITELISTED_EXT_ID: [
+                    test_data.mock["claim"]["with_any_external_id"]
+                ]
+            }
+        },
+        "with_all": {
+            **test_data.ITEM,
+            "claims": {
+                test_data.NON_BLACKLISTED_PROPERTY: [
+                    test_data.mock["claim"]["with_any_unreferenced_property"]
+                ],
+                test_data.WHITELISTED_EXT_ID: [
+                    test_data.mock["claim"]["with_whitelisted_external_id"]
+                ]
+            }
+        }
+    }
+}
+
+expected = {
+    "line": {
+        # TODO: This shouldn't be a valid output, see T251944
+        "without_resource_urls": {
+            **test_data.LINE,
+            "statements": [test_data.STATEMENT_BLOB],
+            "resourceUrls": []
+        },
+        # TODO: This shouldn't be a valid output, see T251944
+        "without_statements": {
+            **test_data.LINE,
+            "statements": []
+        },
+        "with_all": {
+            **test_data.LINE,
+            "statements": [test_data.STATEMENT_BLOB],
+            "resourceUrls": [test_data.RESOURCE_BLOB]
+        }
+    }
+}
 
 
-@pytest.fixture
-def input_data():
-    return json.loads(pkg_resources.read_text(
-        test_data,
-        'item_extractor_pipe_input_test_data.json'
-    ))
+class MockExternalIdFormatter:
+    def format(self, *args):
+        return test_data.RESOURCE_BLOB
 
 
-@pytest.mark.skip
-def test_pipe1_integration(input_data, tmp_path):
-    #  TODO: add test cases covering blacklisted properties and the whitelisted external ids
-    pipe = ItemExtractorPipe((lambda pid, value: {}), ['P26'])
-    result = pipe.flow(input_data)
-    expected_data = json.loads(
-        pkg_resources.read_text(
-            test_data, 'item_extractor_pipe_expected_data.jsonl'
+class TestItemExtractorPipe:
+    @pytest.mark.parametrize("given,expected", [
+        (
+            given["item"]["without_unreferenced_claims"],
+            [expected["line"]["without_statements"]]
+        ),
+        (
+            given["item"]["without_non_blacklisted_properties"],
+            [expected["line"]["without_statements"]]
+        ),
+        (
+            given["item"]["without_whitelisted_external_ids"],
+            [expected["line"]["without_resource_urls"]]
+        ),
+        (
+            given["item"]["with_all"],
+            [expected["line"]["with_all"]]
         )
-    )
-    assert result[0] == expected_data
+    ])
+    def test_flow(self, given, expected):
+        external_id_formatter = MockExternalIdFormatter()
+        blacklisted_properties = [test_data.BLACKLISTED_PROPERTY]
+        whitelisted_ext_ids = [test_data.WHITELISTED_EXT_ID]
+        pipe = ItemExtractorPipe(external_id_formatter, blacklisted_properties, whitelisted_ext_ids)
+
+        assert pipe.flow(given) == expected

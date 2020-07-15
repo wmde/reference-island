35450d6780f6a6f2ef1ac2d96b612a9f448fa27f
Itamar Givon <itamar.givon@wikimedia.de>
2020-07-15 15:07:23 +0200

    Uppercase dump flag

--- wikidata_game/views/stats-page.php
+++ wikidata_game/views/stats-page.php
@@ -25,6 +25,6 @@
             <strong>Acceptance Rate</strong>: <?php echo $acceptance_rate ?>%
         </li>
     </ul>
-    <p><a href="?dump=rejected" download>Download Rejected Matches (CSV)</a></p>
+    <p><a href="?dump=REJECTED" download>Download Rejected Matches (CSV)</a></p>
 </body>
 </html>
